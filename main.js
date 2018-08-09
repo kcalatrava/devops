e3a518b8b803718a2f48477c5e9a6000670507c4
kcalatrava <aufwacht@gmail.com>
2018-08-09 12:48:21 -0400

    Update main.js
    Actualizando mensajes del puerto a desplegar

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("Hello kevinpina.com\n");
 });
 
-// listen on localhost:8000
+// listen on localhost:8080
 server.listen(8080);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Server listening at http://127.0.0.1:8080/");
